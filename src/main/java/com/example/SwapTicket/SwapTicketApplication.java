cd848ae1ac9259285635f20ad650ff556c9e8186
sanat-buyya <sanatbuyya@gmail.com>
2025-06-11 02:29:29 +0530

    Added Logo and CSS updated

--- src/main/java/com/example/SwapTicket/SwapTicketApplication.java
+++ src/main/java/com/example/SwapTicket/SwapTicketApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-
 import com.example.SwapTicket.model.Wallet;
 import com.example.SwapTicket.repository.WalletRepository;
 
