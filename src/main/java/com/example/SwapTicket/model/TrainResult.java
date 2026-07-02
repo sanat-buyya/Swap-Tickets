1273295f83c15b936e56cd809c30b77d0ac9caff
sanat-buyya <sanatbuyya@gmail.com>
2026-07-02 13:28:21 +0530

    live train status api integrated

--- src/main/java/com/example/SwapTicket/model/TrainResult.java
+++ src/main/java/com/example/SwapTicket/model/TrainResult.java
@@ -1,20 +1,24 @@
  package com.example.SwapTicket.model;
 
-public class TrainResult {
+ import java.util.List;
+
+ public class TrainResult {
     private String trainNo;
     private String trainName;
     private String sourceStation;
     private String destinationStation;
     private String departureTime; // from source
     private String arrivalTime;   // at destination
+    private List<String> runDays;
 
-    public TrainResult(String trainNo, String trainName, String sourceStation, String destinationStation, String departureTime, String arrivalTime) {
+    public TrainResult(String trainNo, String trainName, String sourceStation, String destinationStation, String departureTime, String arrivalTime, List<String> runDays) {
         this.trainNo = trainNo;
         this.trainName = trainName;
         this.sourceStation = sourceStation;
         this.destinationStation = destinationStation;
         this.departureTime = departureTime;
         this.arrivalTime = arrivalTime;
+        this.runDays = runDays;
     }
 
     // Getters
@@ -24,4 +28,8 @@ public class TrainResult {
     public String getDestinationStation() { return destinationStation; }
     public String getDepartureTime() { return departureTime; }
     public String getArrivalTime() { return arrivalTime; }
+     public List<String> getRunDays() {
+         return runDays;
+     }
+
 }
